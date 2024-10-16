0e58bef046beb64fe89b52d62280bc080a78ef7f
Hugp <uneetagenonbinaire@gmail.com>
2024-10-16 22:48:50 +0200

    Buildable

--- front/lib/camera.dart
+++ front/lib/camera.dart
@@ -3,16 +3,16 @@ import 'package:camera/camera.dart';
 import 'package:tflite_v2/tflite_v2.dart';
 import 'dart:math' as math;
 
-typedef void Callback(List<dynamic> list, int h, int w);
+typedef Callback = void Function(List<dynamic> list, int h, int w);
 
 class Camera extends StatefulWidget {
   final List<CameraDescription> cameras;
   final Callback setRecognitions;
 
-  Camera(this.cameras, this.setRecognitions);
+  const Camera(this.cameras, this.setRecognitions, {super.key});
 
   @override
-  _CameraState createState() => new _CameraState();
+  _CameraState createState() => _CameraState();
 }
 
 class _CameraState extends State<Camera> {
@@ -23,10 +23,10 @@ class _CameraState extends State<Camera> {
   void initState() {
     super.initState();
 
-    if (widget.cameras == null || widget.cameras.length < 1) {
+    if (widget.cameras.isEmpty) {
       print('No camera is found');
     } else {
-      controller = new CameraController(
+      controller = CameraController(
         widget.cameras[0],
         ResolutionPreset.high,
       );
@@ -40,7 +40,7 @@ class _CameraState extends State<Camera> {
           if (!isDetecting) {
             isDetecting = true;
 
-            int startTime = new DateTime.now().millisecondsSinceEpoch;
+            int startTime = DateTime.now().millisecondsSinceEpoch;
 
             Tflite.detectObjectOnFrame(
               bytesList: img.planes.map((plane) {
@@ -54,7 +54,7 @@ class _CameraState extends State<Camera> {
               numResultsPerClass: 1,
               threshold: 0.2,
             ).then((recognitions) {
-              int endTime = new DateTime.now().millisecondsSinceEpoch;
+              int endTime = DateTime.now().millisecondsSinceEpoch;
               print("Detection took ${endTime - startTime}");
 
               widget.setRecognitions(recognitions!, img.height, img.width);
@@ -69,13 +69,13 @@ class _CameraState extends State<Camera> {
 
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (controller == null || !controller.value.isInitialized) {
+    if (!controller.value.isInitialized) {
       return Container();
     }
 
